3763ab9b6208ec4b9d4dc1018023bbc115f1899b
Taylor Cool <taycool@gmail.com>
2020-02-03 09:25:33 -0800

    fixed seed formatting

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -5,9 +5,24 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'user1', password: 'password', phone_number: '(123)456-7890'},
-        {id: 2, username: 'user2', password: 'password', phone_number: '(098)765-4321'},
-        {id: 3, username: 'user3', password: 'password', phone_number: '(800)123-4567'}
+        {
+          id: 1,
+          username: 'user1',
+          password: 'password', 
+          phone_number: '(123)456-7890'
+        },
+        {
+          id: 2, 
+          username: 'user2',
+          password: 'password', 
+          phone_number: '(098)765-4321'
+        },
+        {
+          id: 3, 
+          username: 'user3', 
+          password: 'password', 
+          phone_number: '(800)123-4567'
+        }
       ]);
     });
 };
